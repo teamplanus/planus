68808923b96228aef26be17eac2dc4caf429c69b
6londe <6londe@gmail.com>
2018-11-15 23:18:38 +0900

    Modify block structure

--- block.go
+++ block.go
@@ -3,8 +3,8 @@ package core
 type Block struct {
 	
 	BlockNumber		uint64
-	PreviousHash	string
+	Hash			[]byte
+	PreviousHash	[]byte
 	TimeStamp		uint64	// Unix timestamp (https://www.unixtimestamp.com/)
 	Data			string	// FIXME It will be changed to some struct contains txs.
-	Hash			string
 }
